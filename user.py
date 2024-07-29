b55f4cb6211fb3e78e18f57d5863cfe118ba025c
Neelesh Pandey <72847037+Neelesh707@users.noreply.github.com>
2024-07-29 12:33:35 +0530

    Delete user.py

--- user.py
+++
@@ -1,9 +0,0 @@
-t=tuple()
-n=int(input("Enter no. of entries : "))
-for i in range(n):
-    a=input("Enter an element : ")
-    t+=(a,)
-for i in t:
-    print(i)
-print("Maximum is : ",max(t))
-print("Minimum is : ",min(t))
